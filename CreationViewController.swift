ed4a5d3f9e6f54480af430d1a47037419694a49f
Mosammad Kabir <97767575+m-kabir02@users.noreply.github.com>
2022-03-21 01:51:46 -0400

    Add files via upload
    Updated Lab 4 With Optionals Code

--- CreationViewController.swift
+++ CreationViewController.swift
@@ -14,12 +14,26 @@ class CreationViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        questionTextField.text = initialQuestion
+        answerTextField.text = initialAnswer
+        
     }
     
     @IBOutlet weak var questionTextField: UITextField!
     
     @IBOutlet weak var answerTextField: UITextField!
+    var initialQuestion: String?
+    var initialAnswer: String?
+    
+    //🤓 Pro tip: The two properties should be String? and not String cuz that way in the case of creating a new flashcard, instead of editing an existing one, it is ok to not have initial values. More specifically, it's ok that our initial values are nil.
+    
+    @IBOutlet weak var extraQuestion1: UITextField!
+    
+    @IBOutlet weak var extraAnswer1: UITextField!
+    
+    @IBOutlet weak var extraQuestion2: UITextField!
+
+    @IBOutlet weak var extraAnswer2: UITextField!
     
     @IBAction func didTapOnCancel(_ sender: Any)
     {
@@ -32,9 +46,33 @@ dismiss(animated: true)
         let questionText = questionTextField.text
         let answerText = answerTextField.text
         
-        flashcardsController.updateFlashcard(question: questionText!, answer: answerText!)
+        let questionText2 = extraQuestion1.text
+        let answerText2 = extraAnswer1.text
         
-        dismiss(animated: true)
+        let questionText3 = extraQuestion2.text
+        let answerText3 = extraAnswer2.text
+        
+        //if there is no question or answer in the text field
+               if questionText == nil || answerText == nil || questionText!.isEmpty || answerText!.isEmpty
+               {
+                   //show error alert
+                   let alert = UIAlertController(title: "Missing Text", message: "You need to add a question and an answer", preferredStyle: .alert)
+                   
+                   present(alert, animated: true)
+                   
+                   let okAction = UIAlertAction(title: "Ok", style: .default)
+                   alert.addAction(okAction)
+                   
+               }
+               else
+               {
+                   flashcardsController.updateFlashcard(question: questionText!, answer: answerText!, extraQuestion1: questionText2, extraAnswer1: answerText2, extraQuestion2: questionText3, extraAnswer2: answerText3)
+               
+               dismiss(animated: true)
+               
+           }
+        
+        
         
     }
     
